97c4ec2ba89cb947dc6a5f4dbcdd8b1533e7a188
shreyas <shreyasacharya256@gmail.com>
2026-07-26 20:07:11 +0530

    Add POST endpoint to create notes

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,32 @@
 from fastapi import FastAPI
+from sqlalchemy.orm import Session
 
-from database import Base, engine
 import models
+import schemas
+from database import Base, SessionLocal, engine
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+
 @app.get("/")
 def home():
     return {"message": "Welcome to Notes API!"}
+
+
+@app.post("/notes")
+def create_note(note: schemas.NoteCreate):
+    db: Session = SessionLocal()
+
+    new_note = models.Note(
+        title=note.title,
+        content=note.content
+    )
+
+    db.add(new_note)
+    db.commit()
+    db.refresh(new_note)
+    db.close()
+
+    return new_note
