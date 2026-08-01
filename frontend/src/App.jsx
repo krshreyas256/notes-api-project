5993dcacb0102fe8bb0e004a0951ce8e43a7562d
shreyas <shreyasacharya256@gmail.com>
2026-08-01 11:39:38 +0530

    Fetch notes from FastAPI on load

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,8 @@ import Header from './components/Header'
 import NoteForm from './components/NoteForm'
 import NoteList from './components/NoteList'
 import Footer from './components/Footer'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios"
 
 function App() {
 	const [notes, setNotes] = useState([])
@@ -12,20 +13,29 @@ function App() {
 		setNotes([...notes, note])
 	}
 
+	async function fetchNotes() {
+		const response = await axios.get("http://127.0.0.1:8000/notes")
+		setNotes(response.data)
+	}
+
+	useEffect(() => {
+		fetchNotes()
+	}, [])
+
 	return (
 		<div>
 			<Header
 				title="Notes Application"
 				subtitle="Welcome to my first React app"
 			/>
-
+			
 			<NoteForm 
 				onAddNote={addNote}	
 			/>
 			<NoteList 
 				notes={notes}
 			/>
-			
+
 			<Footer />
 		</div>
   )
